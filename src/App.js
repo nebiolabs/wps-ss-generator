6a77e35f47b442305d2685957e926df87eb038ff
Harold Bell <harold.william.bell@gmail.com>
2024-03-18 16:17:45 -0400

    reading the files w papaparse, can attempt to assemble now.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Papa from 'papaparse';
 import { initializePlate } from './tools/helperFunctions'
-import SubmissionUploader from './components/SubmissionUploader';
+import { Input, Form, Button } from 'reactstrap';
 
 export default function App() {
 
-  const [files, setFiles] = useState();
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    console.log(files);
+  }, [files]);
 
   function handleChange(event) {
-    setFiles(event.target.files[0]);
-
-    console.log(event.target.files);
-    console.log(event.target.files.length);
-
+    let uploadedFiles = [];
 
     for (let i = 0; i < event.target.files.length; i++) {
+      let fileName = event.target.files[i].name;
+      console.log(fileName);
       Papa.parse(event.target.files[i], {
         complete: function (results) {
-          console.log(`File ${i}`, results.data);
+          uploadedFiles.push({ name: fileName, samples: results.data });
+          setTimeout(() => {
+            setFiles(uploadedFiles)
+          }, 500)
         }
       });
+
     }
+  }
 
+  function handleClearFiles() {
+    setFiles([])
+  }
 
+  function handleMakeSS() {
+    console.log(files)
   }
 
   return (
     <div className="App">
-      <form>
-        <h1>React File Upload</h1>
-        <input type="file" multiple onChange={handleChange} />
-        <button type="submit">Upload</button>
-      </form>
+      <div className="uploader flex-start-col">
+        <h2 style={{ color: '#1F618D', fontFamily: 'Montserrat' }}>Load all WPS submissions below</h2>
+        <Input type="file" multiple onChange={handleChange} style={{ margin: '10px 0' }} />
+
+        {files.map(file => <p>{file.name}</p>)}
+        {/* <Button color="warning" onClick={handleClearFiles}>clear selections</Button> */}
+        <Button color="primary" onClick={handleMakeSS}>make SS</Button>
+      </div>
+
+
+
     </div>
   );
 }
--- src/tools/helperFunctions.js
+++ src/tools/helperFunctions.js
@@ -1,7 +1,8 @@
 
 function initializePlate() {
-  console.log('building 96 well plate ... ');
+  console.log('initializing 96 well plate ... ');
 
+  let headers = ['Total Samples', 'Submitter Name', 'Email', 'Sample #', 'Sample Name', 'Qubit Concentration ng/ul', 'Expected Size (bases)', 'Read Count', 'Vector Backbone (optional)', 'Reference Genome', 'Barcode Well Position', 'Barcode Well #', 'Volume Sample (ul)', 'Volume H2O (ul)'];
 
 }
 
