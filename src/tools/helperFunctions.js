29c5e744a1d5d4a144b40002528e5625b8160058
Harold Bell <harold.william.bell@gmail.com>
2024-03-25 14:59:15 -0400

    starting to make the plate loading logic more complex

--- src/tools/helperFunctions.js
+++ src/tools/helperFunctions.js
@@ -7,11 +7,10 @@ function initializePlate() {
 
   let headers = ['Total Samples', 'Submitter Name', 'Email', 'Sample #', 'Sample Name', 'Qubit Concentration ng/ul', 'Expected Size (bases)', 'Read Count', 'Vector Backbone (optional)', 'Reference Genome', 'Barcode Well Position', 'Barcode Well #', 'Volume Sample (ul)', 'Volume H2O (ul)'];
 
-  let cols = [...Array(10).keys()];
   let rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
   let masterPlate = [headers];
 
-  // set up the scaffold
+  // set up the scaffold of 96 rows
   for (let i = 0; i < 96; i++) {
     let masterPlateRow = Array(headers.length).fill('');
     let plateCol = Math.floor(i / 8) + 1;
@@ -19,7 +18,6 @@ function initializePlate() {
     masterPlateRow[0] = i + 1;
     masterPlateRow[7] = '10000';
     masterPlateRow[10] = `${plateRow}${plateCol}`;
-
     masterPlate.push(masterPlateRow);
   }
   // console.log(masterPlate);
@@ -39,10 +37,14 @@ function extractSamples(submission) {
   return samplesInfo;
 }
 
+// this function just puts all the submissions together, no matter how many. later, separate into 96 well plates.
 function makeSampleSheet(submissions) {
   if (!Array.isArray(submissions)) {
     return;
   }
+
+  // sort by # of samples first
+  submissions.sort((a, b) => b - a);
 
   console.log('making sample sheet ... ')
   let masterPlate = initializePlate();
@@ -50,11 +52,19 @@ function makeSampleSheet(submissions) {
 
   submissions.forEach((submission) => {
     let samples = extractSamples(submission);
+
+    // is there room for this one?
+    let spaceLeft = masterPlate.filter((row) => row[4] === '').length;
+    console.log(`${spaceLeft} wells left ... checking for room`)
+    if (spaceLeft - samples.length < 0) {
+      return alert(`There isn't enough room for this order: ${submission.name}. Try selecting a different group of orders.`)
+    }
+
     console.log(samples)
     // where does this sample get added? 
     let startingLocation = 1;
     for (let i = 1; i < masterPlate.length - 1; i++) {
-      if (masterPlate[i][4] === '') {
+      if (masterPlate[i][4] === '' && masterPlate[i][10].includes('A')) {
         startingLocation = i;
         break;
       }
@@ -74,15 +84,50 @@ function makeSampleSheet(submissions) {
 
     //   masterPlate[startingLocation][j + 1] = samples[j];
 
-    console.log(masterPlate)
+    // console.log(masterPlate)
 
 
 
   });
 
 
-  // console.log(masterPlate)
+  console.log(masterPlate)
   return masterPlate;
+}
+
+// this function should find empty space in the plate compatible with the # of samples.
+// Ex: D5 --> H5 is empty, so can fit <= 5 samples here without jumping to the next empty A well.
+// Note: we don't want the samples loading 
+// should return array of [ startWell, endWell ]
+function findAvailableSpace(plate, samplesLength) {
+  let spaceLength = 0;
+  let startWell = '';
+  let endWell = '';
+
+  for (let i = 0; i < plate.length; i++) {
+
+    if (plate[i][4] === '') {
+      // good, this well is empty. keep evaluating
+
+      //only assign startingWell when it is the first empty spot
+      if (spaceLength === 0) {
+        startWell = plate[i][10];
+      }
+      spaceLength = spaceLength++;
+    } else {
+      // bad, this well is occupied. go back to starting values
+      spaceLength = 0;
+      startWell = '';
+    }
+
+    // alright, we have enough space 
+    if (spaceLength === samplesLength) {
+      endWell = plate[i][10];
+      break;
+    }
+  }
+
+  return [startWell, endWell];
 }
 
 module.exports = {
